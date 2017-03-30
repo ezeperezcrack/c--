160762e214937f214c90931a1199e0504c7c1cb3
ezeperezcrack <ezequielda.perez@outlook.com>
2017-03-29 22:07:28 -0300

    Add files via upload

---
+++ Eze/Eze/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var=0,i=0,max=0,min=0, acumulador=0;
+            float promedio;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("ingrese un numero");
+                var = int.Parse(Console.ReadLine());
+                acumulador = acumulador + var;
+                if (i == 0)
+                {
+                    max = var;
+                    min = var;
+                }
+                else if (var > max)
+                {
+                    max = var;
+                }
+                else if (var < min)
+                {
+                    min = var;
+                }
+            }
+            promedio = (float)acumulador / 5;
+            Console.WriteLine("el maximo es {0} el minimo es {1} el promedio es {2}", max,min,promedio);
+            Console.Read();
+        }
+    }
+}
---
+++ Eze/PerezEzequielEjercicio02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerezEzequielEjercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int var = 10, i=0, h=0, color =0;
+           /* Console.WriteLine("ingrese un numero");
+            var = int.Parse(Console.ReadLine());
+            */
+            for(i=0;i<var;i++)
+            {
+                if (color >2)
+                    color = 0;
+
+                switch (color)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                }
+                    if (i > 0)
+                    {
+                        for (h = 0; h < i; h++)
+                        {
+                            Console.Write("**");
+                        }
+                    }
+                Console.WriteLine("*");
+                color++;
+            }
+            Console.Read();
+        }
+    }
+}
