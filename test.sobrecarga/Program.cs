57db5d1046e3da296f6df022194f477822a34c7f
ezeperezcrack <ezequielda.perez@outlook.com>
2017-04-05 21:26:05 -0300

    Add files via upload
    sobrecarga

---
+++ test.sobrecarga/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using entidades.sobrecarga;
+//ConsoleColor.DarkBlue, Etipo.conbrillito 
+
+
+
+namespace test.sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            entidades.sobrecarga.tinta a= new entidades.sobrecarga.tinta();
+            entidades.sobrecarga.tinta b = new entidades.sobrecarga.tinta(ConsoleColor.DarkBlue);
+            entidades.sobrecarga.tinta c = new entidades.sobrecarga.tinta(ConsoleColor.DarkGreen,Etipo.conbrillito);
+            Pluma plum = new Pluma("faber", 1, a);
+            plum = plum + a;
+
+
+            //operador "explicit" defino una tinta sin instanciar (" tinta t ") y asigno t= (tinta)plum
+            //operador "-" plum = plum - a;  
+
+            Console.WriteLine(plum);
+           if (plum == a)
+           {
+               Console.WriteLine("son iguales");
+           }
+           else
+           {
+               Console.WriteLine("son distintas");
+           }
+            
+           Console.ReadLine();
+
+
+        }
+    }
+}
